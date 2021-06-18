e0ca6dc620a0fefe94379b0c4ffc948f9c7822dc
choiyh <choiyh44@gmail.com>
2021-06-18 10:56:12 +0900

    소스보완

--- src/main/java/kr/co/ensmart/frameworkdemo/FrameworkDemoMybatisApplication.java
+++ src/main/java/kr/co/ensmart/frameworkdemo/FrameworkDemoMybatisApplication.java
@@ -3,10 +3,8 @@ package kr.co.ensmart.frameworkdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({ "kr.co.ensmart.frameworkdemo" })
 @MapperScan(basePackages = "kr.co.ensmart.frameworkdemo.app.dao")
 public class FrameworkDemoMybatisApplication {
 
